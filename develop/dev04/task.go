f4b1fc019c69461a5d00681057c4c849507849fa
Owner-maker <lisitsyn.artem.d@gmail.com>
2023-02-17 16:09:54 +0400

    Add 4 task solution

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"hash/fnv"
+	"sort"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +26,67 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func getByteSliceHash(bytes []byte) uint32 {
+	h := fnv.New32a()
+	h.Write(bytes)
+	return h.Sum32()
+}
 
+func parseAndSortStringToBytes(word string) []byte {
+	bytes := []byte(word)
+	sort.Slice(
+		bytes,
+		func(i, j int) bool {
+			return (bytes)[i] < (bytes)[j]
+		})
+	return bytes
+}
+
+func getMapWithHashKeyWords(words []string) map[uint32][]string {
+	res := make(map[uint32][]string, len(words))
+	var hash uint32
+
+	for _, v := range words {
+		hash = getByteSliceHash(parseAndSortStringToBytes(v))
+		if _, found := res[hash]; !found {
+			res[hash] = make([]string, 0)
+			res[hash] = append(res[hash], v)
+			continue
+		}
+		res[hash] = append(res[hash], v)
+	}
+	return res
+}
+
+func makeStringsLowercase(words []string) []string {
+	for i, v := range words {
+		words[i] = strings.ToLower(v)
+	}
+	return words
+}
+
+func getConvertedMap(hashMap map[uint32][]string) map[string][]string {
+	res := make(map[string][]string, len(hashMap))
+	var t []string
+
+	for _, v := range hashMap {
+		if len(v) <= 1 {
+			continue
+		}
+		t = v[1:]
+		t = makeStringsLowercase(t)
+		sort.Strings(t)
+		res[v[0]] = t
+	}
+	return res
+}
+
+func getAnagrams(words []string) map[string][]string {
+	return getConvertedMap(getMapWithHashKeyWords(words))
+}
+
+func main() {
+	a := []string{"пятак", "листок", "слиток", "пятка", "тяпка", "столик", "кошка", "шокка"}
+	r := getAnagrams(a)
+	fmt.Println(r)
 }
