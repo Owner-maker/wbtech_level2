3cc733aa3e426d75cf7d7c6745fbe5eeadbb1f41
Owner-maker <lisitsyn.artem.d@gmail.com>
2023-02-18 02:08:42 +0400

    Add 7 task solution

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -33,6 +39,43 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	res := make(chan interface{})
+	defer close(res)
+
+	wg := sync.WaitGroup{}
+
+	for _, ch := range channels {
+		wg.Add(1)
+		go func(ch <-chan interface{}) {
+			for i := range ch {
+				res <- i
+			}
+			wg.Done()
+		}(ch)
+	}
+	wg.Wait()
+
+	return res
+}
+
 func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(15*time.Second),
+		sig(1*time.Hour),
+		sig(10*time.Second),
+	)
+	fmt.Printf("fone after %v", time.Since(start))
 
 }
