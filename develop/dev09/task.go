63cbfbba10cdd212ed3c9828dd867034e53e92e6
Owner-maker <lisitsyn.artem.d@gmail.com>
2023-02-18 17:51:32 +0400

    Add 9 task solution and minor changes

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
 /*
 === Утилита wget ===
 
@@ -8,6 +16,44 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+const fileOutput = "output.txt"
 
+func writeHtmlBodyToFile(file string, response http.Response) error {
+	data, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer func(data *os.File) {
+		err = data.Close()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}(data)
+
+	writer := bufio.NewWriter(data)
+	_, err = io.Copy(writer, response.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func wget(file, url string) error {
+	r, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	err = writeHtmlBodyToFile(file, *r)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func main() {
+	url := "https://owner-maker.github.io/"
+	err := wget(fileOutput, url)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- develop/go.mod
+++
@@ -1,3 +0,0 @@
-module develop
-
-go 1.19
